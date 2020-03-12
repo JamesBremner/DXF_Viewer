195ac80aa82352649685371502c3c50144809470
James Bremner <james@ravenspoint.com>
2020-03-12 10:22:14 -0400

    Fix unit test #15

--- src/unitTest.cpp
+++ src/unitTest.cpp
@@ -14,33 +14,40 @@ int main()
     A.r = 50;           // radius 50
     dxfv::cBoundingRectangle BR;
 
-    std::cout << "bottom right quadrant\n";
+    std::cout << "top right quadrant\n";
     A.sa = 5;
     A.ea = 85;
     BR.init = false;
     A.Update( BR );
     std::cout << BR.x1<<" "<<BR.y1<<", "<<BR.x2<<" "<<BR.y2<<"\n";
+    if( BR.x1 < 100 || BR.y1 < 100 || BR.x2 < 100 || BR.y2 < 100 )
+        std::cout << "FAILED\n";
 
-    std::cout << "bottom left quadrant\n";
+    std::cout << "top left quadrant\n";
     A.sa = 95;
     A.ea = 175;
     BR.init = false;
     A.Update( BR );
     std::cout << BR.x1<<" "<<BR.y1<<", "<<BR.x2<<" "<<BR.y2<<"\n";
+    if( BR.x1 > 100 || BR.y1 < 100 || BR.x2 > 100 || BR.y2 < 100 )
+        std::cout << "FAILED\n";
 
-    std::cout << "top left quadrant\n";
+    std::cout << "bottom right quadrant\n";
     A.sa = 185;
     A.ea = 265;
     BR.init = false;
     A.Update( BR );
     std::cout << BR.x1<<" "<<BR.y1<<", "<<BR.x2<<" "<<BR.y2<<"\n";
+    if( BR.x1 > 100 || BR.y1 > 100 || BR.x2 > 100 || BR.y2 > 100 )
+        std::cout << "FAILED\n";
 
-    std::cout << "top right quadrant\n";
-    A.sa = 375;
+    std::cout << "bottom left quadrant\n";
+    A.sa = 275;
     A.ea = 355;
     BR.init = false;
     A.Update( BR );
     std::cout << BR.x1<<" "<<BR.y1<<", "<<BR.x2<<" "<<BR.y2<<"\n";
-
+    if( BR.x1 < 100 || BR.y1 > 100 || BR.x2 < 100 || BR.y2 > 100 )
+        std::cout << "FAILED\n";
     return 0;
 }
