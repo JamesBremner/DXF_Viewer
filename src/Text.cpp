2d65a935dde15f2077e7d53547b17f5db37ef22b
JamesBremner <james@ravenspoint.com>
2024-08-17 10:31:34 -0400

    Colored text #30

--- src/Text.cpp
+++ src/Text.cpp
@@ -2,139 +2,139 @@
 #include "DXF.h"
 #include "Text.h"
 
-
 namespace dxfv
 {
 
-CText::CText()
-    : cDXFGraphObject("MTEXT",cDXFGraphObject::eType::text)
-{
-
-}
-CText::CText( cCodeValue& cv )
-    : CText()
-{
-    myfValid =( cv.myValue == myCode );
-}
-
-bool CText::Append(  cvit_t& cvit )
-{
-    while( true )
+    CText::CText()
+        : cDXFGraphObject("MTEXT", cDXFGraphObject::eType::text)
     {
-        cvit++;
-        switch( cvit->Code() )
+    }
+    CText::CText(cCodeValue &cv)
+        : CText()
+    {
+        myfValid = (cv.myValue == myCode);
+    }
+
+    bool CText::Append(cvit_t &cvit)
+    {
+        while (true)
         {
-        case 0:
-            // a new object
-            cvit--;
+            cvit++;
+            switch (cvit->Code())
+            {
+            case 0:
+                // a new object
+                cvit--;
+                return false;
+            case 1:
+                // the text to display
+                myText = cvit->myValue;
+                //            if( myText.find("{\\f") != -1 ) {
+                //                int p = myText.find(";");
+                //                if( p == -1 )
+                //                    return false;
+                //                myText = myText.substr(p+1);
+                //            }
+                clean_mtext(myText);
+                break;
+            case 10:
+                x1 = atof(cvit->myValue.c_str());
+                break;
+            case 20:
+                y1 = atof(cvit->myValue.c_str());
+                break;
+            case 62:
+                AutocadColor2RGB(atoi(cvit->myValue.c_str()));
+                break;
+            }
+        }
+        return true;
+    }
+
+    bool CText::getDraw(cDrawPrimitiveData &draw)
+    {
+        if (draw.index)
             return false;
-        case 1:
-            // the text to display
-            myText = cvit->myValue;
-//            if( myText.find("{\\f") != -1 ) {
-//                int p = myText.find(";");
-//                if( p == -1 )
-//                    return false;
-//                myText = myText.substr(p+1);
-//            }
-            clean_mtext( myText );
-            break;
-        case 10:
-            x1 = atof(cvit->myValue.c_str());
-            break;
-        case 20:
-            y1 = atof(cvit->myValue.c_str());
-            break;
-        }
-
+        draw.index++;
+        draw.text = myText;
+        draw.x1 = x1;
+        draw.y1 = y1;
+        draw.rect->ApplyScale(draw.x1, draw.y1);
+        draw.color = myColor;
+        return true;
     }
-    return true;
-}
 
-
-bool CText::getDraw( cDrawPrimitiveData& draw )
-{
-    if( draw.index )
-        return false;
-    draw.index++;
-    draw.text = myText;
-    draw.x1 = x1;
-    draw.y1 = y1;
-    draw.rect->ApplyScale(draw.x1, draw.y1);
-    return true;
-}
-
-void CText::clean_mtext( std::string& text )
-{
-    int esc = 0;
-    while( 1 )
+    void CText::clean_mtext(std::string &text)
     {
-        esc = text.find("\\",esc);
-        if( esc == -1 )
-            break;
-        if( text[esc+1] == 'f' )
+        int esc = 0;
+        while (1)
         {
-            int p = text.find(";",esc+1);
-            if( p == -1 )
+            esc = text.find("\\", esc);
+            if (esc == -1)
+                break;
+            if (text[esc + 1] == 'f')
             {
-                text = "";
-                return;
+                int p = text.find(";", esc + 1);
+                if (p == -1)
+                {
+                    text = "";
+                    return;
+                }
+                text = text.substr(0, esc) + text.substr(p + 1);
             }
-            text = text.substr(0,esc) + text.substr(p+1);
-        }
-        else if ( text[esc+1] == 'L' )
-        {
-            text = text.substr(0,esc) + text.substr(esc+2);
-        }
-        else if ( text[esc+1] == 'P' )
-        {
-            text = text.substr(0,esc) + "\n" + text.substr(esc+2);
-        }
-        else if ( text[esc+1] == 'A' )
-        {
-            text = text.substr(0,esc) + text.substr(esc+2);
-        }
-        else if ( text[esc+1] == 'p' )
-        {
-            int p = text.find(";",esc+1);
-            if( p == -1 )
+            else if (text[esc + 1] == 'L')
             {
-                text = "";
-                return;
+                text = text.substr(0, esc) + text.substr(esc + 2);
             }
-            text = text.substr(0,esc) + text.substr(p+1);
-        }
-        else if ( text[esc+1] == 'H' )
-        {
-            int p = text.find(";",esc+1);
-            if( p == -1 )
+            else if (text[esc + 1] == 'P')
             {
-                text = "";
-                return;
+                text = text.substr(0, esc) + "\n" + text.substr(esc + 2);
             }
-            text = text.substr(0,esc) + text.substr(p+1);
-        }
-        else if ( text[esc+1] == 'S' )
-        {
-            int p = text.find(";",esc+1);
-            if( p == -1 )
+            else if (text[esc + 1] == 'A')
             {
-                text = "";
-                return;
+                text = text.substr(0, esc) + text.substr(esc + 2);
             }
-            text = text.substr(0,esc) + text.substr(p+1);
+            else if (text[esc + 1] == 'p')
+            {
+                int p = text.find(";", esc + 1);
+                if (p == -1)
+                {
+                    text = "";
+                    return;
+                }
+                text = text.substr(0, esc) + text.substr(p + 1);
+            }
+            else if (text[esc + 1] == 'H')
+            {
+                int p = text.find(";", esc + 1);
+                if (p == -1)
+                {
+                    text = "";
+                    return;
+                }
+                text = text.substr(0, esc) + text.substr(p + 1);
+            }
+            else if (text[esc + 1] == 'S')
+            {
+                int p = text.find(";", esc + 1);
+                if (p == -1)
+                {
+                    text = "";
+                    return;
+                }
+                text = text.substr(0, esc) + text.substr(p + 1);
+            }
+            else
+            {
+                esc++;
+            }
         }
-        else
-        {
-            esc++;
-        }
+        if (text[0] == '{')
+            text = text.substr(1);
+        if (text[text.length() - 1] == '}')
+            text = text.substr(0, text.length() - 1);
     }
-    if( text[0] == '{' )
-        text = text.substr(1);
-    if( text[ text.length()-1] == '}' )
-        text = text.substr(0,text.length()-1);
-}
-    void CText::Adjust( double ax, double ay )
+    void CText::Adjust(double ax, double ay)
     {
         x1 += ax;
         y1 += ay;
